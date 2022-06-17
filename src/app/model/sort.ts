f9bf5cf35ca1cb7619f4cd67f2005f9235941444
Abenezer <abenitrust@gmail.com>
2022-06-17 14:54:02 -0500

    Minor bug fix on users component
    - Fix table headers
    - Fix bugs related to users table sorting

--- src/app/model/sort.ts
+++ src/app/model/sort.ts
@@ -3,6 +3,9 @@ export class  Sort {
     name: number = 0;
     partNumber: number = 0;
     price: number = 0
+    firstName: number = 0;
+    lastName: number = 0;
+    email: number = 0;
     sortBy: SortKey = "name";
     
 
@@ -11,23 +14,50 @@ export class  Sort {
         return sortValue === 1? "▲" : (sortValue === -1 ? "▼": "");
     }
 
-    setSortBy(sortBy: SortKey) {
-        this.sortBy = sortBy;
+    setSortBy(sortBy: SortKey) {        
+        let temp: number = 0;
         switch(sortBy) {
           case 'name':
+            temp = this['name'];
+            this.clear();
+            this.sortBy = sortBy;
+            this['name'] = temp;
             this['name'] = this.getNextSort();
-            this['partNumber'] = 0;
-            this['price'] = 0;
             break;
           case 'partNumber':
-            this['name'] = 0;
+            temp = this['partNumber'];
+            this.clear();
+            this.sortBy = sortBy;
+            this['partNumber'] = temp;
             this['partNumber'] = this.getNextSort();
-            this['price'] = 0;
             break;
           case 'price':
-            this['name'] = 0;
-            this['partNumber'] = 0;
+            temp = this['price'];
+            this.clear();
+            this.sortBy = sortBy;
+            this['price'] = temp;
             this['price'] = this.getNextSort();
+            break;
+        case 'firstName':
+            temp = this['firstName'];
+            this.clear();
+            this.sortBy = sortBy;
+            this['firstName'] = temp;
+            this['firstName'] = this.getNextSort();
+            break;
+        case 'lastName':
+            temp = this['lastName'];
+            this.clear();
+            this.sortBy = sortBy;
+            this['lastName'] = temp;
+            this['lastName'] = this.getNextSort();
+            break;
+        case 'email':
+            temp = this['email'];
+            this.clear();
+            this.sortBy = sortBy;
+            this['email'] = temp;
+            this['email'] = this.getNextSort();
             break;
         }
     }
@@ -50,13 +80,17 @@ export class  Sort {
     }
 
     sortExists(): boolean {
-        return this.name !== 0 || this.partNumber !== 0 || this.price !== 0;
+        return this.name !== 0 || this.partNumber !== 0 || this.price !== 0
+                || this.firstName !== 0 || this.lastName !== 0 || this.email !==0;
     }
 
     clear(): void {
         this.name = 0;
         this.partNumber = 0;
-        this.price = 0
+        this.price = 0;
+        this.firstName = 0;
+        this.lastName = 0;
+        this.email = 0
         this.sortBy  = "name";
     }
 }
